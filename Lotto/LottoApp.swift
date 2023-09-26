4a28702f8af5535f9d53677af7312162a66eed82
hugesilver <xor3160@naver.com>
2023-09-26 15:19:46 +0900

    Initial Commit

---
+++ Lotto/LottoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottoApp.swift
+//  Lotto
+//
+//  Created by 김태은 on 2023/09/26.
+//
+
+import SwiftUI
+
+@main
+struct LottoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
